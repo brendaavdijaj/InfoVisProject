8c9547e9acbd121f424fc1369238fbc53b37bf31
Njomza Avdijaj <avdijaj@cip.ifi.lmu.de>
2016-12-12 14:20:04 +0100

    Daten

--- InDB/transm.py
+++ InDB/transm.py
@@ -2,25 +2,26 @@ import sqlite3
 import os
 import json
 import datetime as d
+import time
 
 
-def read_folder():
+def read_folder(name):
     """return json names"""
-    json_files = os.listdir("JSON")
-    return json_files
+    json_folders = os.listdir(name)
+    return json_folders
 
 
-def read_one(f):
+def read_one(f, filename):
     """reades one file and saves it"""
-    with open("JSON/" + f) as bike_json:
+    with open("JSON/" + filename + "/" + f) as bike_json:
         b_dict = json.load(bike_json)  # b_dict ist das dokument als dict
         return b_dict
 
 
-def filter(j_dict, date):
+def filter(j_dict, ts):
     """ Filters Latitude, Longitude and Bike ID from JSON file"""
 
-    date = str(date).split("date")[0]
+    ts = int(ts)
     markers = j_dict["marker"]
     data_tuples = []
     for marker in markers:
@@ -33,7 +34,7 @@ def filter(j_dict, date):
         streets = str(street.split("/"))
         for bike in bikelist:
             data_tuples.append((int(bike["Number"]), streets, latitude,
-                                longitude, date))
+                                longitude, ts))
     return data_tuples
 
 
@@ -41,12 +42,23 @@ def makedate(city_ts):
     """ Filters date from file name"""
     splitted_city_ts = city_ts.split("-")
     # city = splitted_city_ts[0]  not necessary
-    year = int(splitted_city_ts[1])
-    month = int(splitted_city_ts[2])
-    day = int(splitted_city_ts[3].split("T")[0])
+    year = splitted_city_ts[1]
+    month = splitted_city_ts[2]
+    day = splitted_city_ts[3].split("T")[0]
     '''Building timestamp out of year, month and day'''
-    date = d.date(year, month, day)
-    return date
+    time_part = splitted_city_ts[-1].split("T")[1].split("+")[0]
+    time_part = time_part.split("_")
+    hours = time_part[0]
+    minutes = time_part[1]
+    seconds = time_part[2]
+
+    t_d_str = year + "-" + month + "-" + day + " " + hours + ":" + minutes +\
+              ":" + seconds
+    print(t_d_str)
+    date_time = d.datetime.strptime(t_d_str, '%Y-%m-%d %H:%M:%S')
+    ts = time.mktime(date_time.timetuple())
+
+    return ts
 
 
 class DBClass():
@@ -69,12 +81,14 @@ class DBClass():
 
 if __name__ == '__main__':
     mydb = DBClass()
-    json_files = read_folder()
-    for f in json_files:
-        city_ts = f
-        j_dict = read_one(f)
-        date = makedate(city_ts)
-        data_tuples = filter(j_dict, date)
-        mydb.insert(data_tuples)
-        print("-------------------------------------Commit done---------------"
-              "--------------------")
+    json_folders = read_folder('JSON')
+    for f in json_folders:
+        json_files = read_folder('JSON/' + f)
+        for e in json_files:
+            city_ts = e
+            j_dict = read_one(e, f)
+            ts = makedate(city_ts)
+            data_tuples = filter(j_dict, ts)
+            mydb.insert(data_tuples)
+            print("-------------------------------------Commit done---------------"
+                  "--------------------")
